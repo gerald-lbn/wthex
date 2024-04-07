3b064bc7379ab6817e409be15711594bc729bf88
Gérald Leban <gerald.leban0@gmail.com>
2024-04-07 20:46:31 +0200

    feat: add basic styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../styles/globals.scss';
+</script>
+
+<main class="container">
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		margin-top: var(--space-6xl);
+		margin-bottom: var(--space-6xl);
+	}
+</style>
