e5ed6a043cb119a7f237759e0a5be91d9117655f
Gérald Leban <gerald.leban0@gmail.com>
2024-04-08 22:30:26 +0200

    feat(i18n): Add translations

--- src/lib/helpers/date.ts
+++ src/lib/helpers/date.ts
@@ -1,4 +1,14 @@
+import { type AvailableLanguageTag } from '$lib/paraglide/runtime';
+
 export const formatDateWithDash = (date: string) =>
 	new Date(date)
 		.toLocaleDateString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' })
 		.replace(/\//g, '-');
+
+export const verbalDate = (date: string, lang: AvailableLanguageTag) => {
+	return new Date(date).toLocaleDateString(lang === 'fr' ? 'fr-FR' : 'en-US', {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	});
+};
