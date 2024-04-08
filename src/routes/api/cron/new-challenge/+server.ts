b51b18f162cd21b9a6c95d9bacded2e7fb09bec7
Gérald Leban <gerald.leban0@gmail.com>
2024-04-08 18:24:08 +0200

    feat: Add cron job to create a new chall every day

---
+++ src/routes/api/cron/new-challenge/+server.ts
@@ -0,0 +1,14 @@
+import { CRON_SECRET } from '$env/static/private';
+import { createChallenge } from '$lib/helpers/challenges';
+
+export async function GET({ request }) {
+	if (request.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`) {
+		return new Response('Unauthorized', { status: 401 });
+	}
+
+	const chall = await createChallenge(3, new Date());
+
+	return new Response(JSON.stringify(chall), {
+		status: 200
+	});
+}
